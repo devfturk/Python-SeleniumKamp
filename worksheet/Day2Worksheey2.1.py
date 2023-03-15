ee2dbd2440cb96f285c07a960be9fe87bff646a4
devfturk <furkanturk0@outlook.com.tr>
2023-03-16 00:32:16 +0300

    Ders2 Ödev2 yapıldı.

---
+++ worksheet/Day2Worksheey2.1.py
@@ -0,0 +1,13 @@
+class Person():
+    def __init__(self,name,lastName):
+        self.name = name
+        self.lastName = lastName
+        
+
+musteri1 = Person("Furkan","Türk")
+musteri2 = Person("Kasım","Türk")
+musteri3 = Person("Hatice","Türk")
+
+print(musteri1.name)
+
+
